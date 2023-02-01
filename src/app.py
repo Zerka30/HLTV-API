4091f56e702f4c255ee82c5cf600fc2ae3241264
Zerka30 <contact.zerka@gmail.com>
2023-02-01 12:44:21 +0100

    :rocket: Adding team route

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 from flask import Flask, jsonify
 
 import config
@@ -33,6 +34,74 @@ def get_news():
         )
 
     return jsonify(rss), 200
+
+
+# Route to get team data
+@app.route("/team/<string:team_id>", methods=["GET"])
+def get_team_date(team_id):
+    try:
+        headers = {"User-Agent": config.USER_AGENT}
+        response = requests.get(
+            config.BASE_URL + "/team/" + team_id + "/_", headers=headers
+        )
+        body = response.text
+
+        soup = BeautifulSoup(body, "html.parser")
+        team_profile = soup.select_one(".teamProfile")
+
+        if not team_profile:
+            return (
+                jsonify(
+                    {
+                        "status": "error",
+                        "message": "There is no team available, something went wrong.",
+                    }
+                ),
+                404,
+            )
+
+        lineup = team_profile.select(".bodyshot-team > a")
+
+        players = []
+        for player in lineup:
+            country_name = player.select_one(".flag")["title"]
+            country_flag = f"{config.BASE_URL}{player.select_one('.flag')['src']}"
+
+            players.append(
+                {
+                    "fullname": player.select_one("img")["title"],
+                    "image": player.select_one("img")["src"],
+                    "nickname": player["title"],
+                    "country": {"name": country_name, "flag": country_flag}
+                    if country_name
+                    else None,
+                }
+            )
+
+        name = team_profile.select_one(".profile-team-name").text
+        logo = team_profile.select_one(".teamlogo")["src"]
+
+        stats_container = team_profile.select(".profile-team-stats-container > div")
+        ranking = int(stats_container[0].select_one(".right").text.replace("#", ""))
+        average_player_age = float(stats_container[2].select_one(".right").text)
+        coach = stats_container[3].select_one(".right").text.strip()
+
+        return (
+            jsonify(
+                {
+                    "id": team_id,
+                    "name": name,
+                    "logo": logo,
+                    "ranking": ranking,
+                    "coach": coach,
+                    "average_player_age": average_player_age,
+                    "players": players,
+                }
+            ),
+            200,
+        )
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 
 # Route for monitoring a container
